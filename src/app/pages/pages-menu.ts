0847d65b02968b5b59d96abe3f06378fbbd06161
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-03-29 13:24:11 +0200

    Added empty project

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,45 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'IoT Dashboard',
+    icon: 'nb-home',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
+    title: 'Tables',
+    icon: 'nb-tables',
+    children: [
+      {
+        title: 'Smart Table',
+        link: '/pages/tables/smart-table',
+      },
+    ],
+  },
+  {
+    title: 'Auth',
+    icon: 'nb-locked',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
+];
