f07f8bef693b24d374a1bc0ac069ad5408ac0abd
Viktor Sergijenko <viktor.sergijenko@bbit.tech>
2019-04-08 19:41:04 +0300

    Imported modules

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
-import { NgxEchartsModule } from 'ngx-echarts';
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
 import { WarehousesListComponent } from './warehouses/warehouses-list/warehouses-list.component';
 import { NbSpinnerModule } from '@nebular/theme';
+import { CommonModule } from '@angular/common';
+import { AddWarehouseModalComponent } from './warehouses/add-warehouse-modal/add-warehouse-modal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditWarehouseModalComponent } from './warehouses/edit-warehouse-modal/edit-warehouse-modal.component';
+
 
 
 
 @NgModule({
   imports: [
+    CommonModule,
     ThemeModule,
-    NgxEchartsModule,
-    NbSpinnerModule
+    NbSpinnerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    NgbDropdownModule
   ],
-  declarations: [DashboardComponent, WarehousesListComponent]
+  declarations: [DashboardComponent, WarehousesListComponent, AddWarehouseModalComponent, EditWarehouseModalComponent],
+  exports: [AddWarehouseModalComponent, EditWarehouseModalComponent],
+  entryComponents: [AddWarehouseModalComponent, EditWarehouseModalComponent]
 })
 
 export class DashboardModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
+
 
 const PAGES_COMPONENTS = [
   PagesComponent,
@@ -13,7 +13,7 @@ const PAGES_COMPONENTS = [
   imports: [
     PagesRoutingModule,
     ThemeModule,
-    DashboardModule,
+    DashboardModule
   ],
   declarations: [
     ...PAGES_COMPONENTS,
